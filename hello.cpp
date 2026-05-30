2842e613223af4906fdd96278b3bf37051a09c7a
ashwanthnk <ashwanthnk5@gmail.com>
2026-05-30 17:08:55 +0530

    Learnt basics

---
+++ hello.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+//using namespace std;
+
+//typedef std::string str_t; (creates alias)
+
+//using str_t = std::string; (create alias)
+
+//namespace first {
+//  int x = 1;
+//}
+
+int main(){
+  /*
+  int a = 0;
+  int b = 1;
+  int c = a+b;
+  int file_size = 100;
+  double sales = 9.99;
+  a++;
+  ++b;
+  const int z = 2;
+  cout << "c: " << c << endl
+       << "sales: " << sales;
+  return 0;
+
+
+  int sales = 95000;
+  double sales_tax = 0.04 * sales;
+  double county_tax = 0.02 * sales;
+  cout << "Sales tax: " << sales_tax << endl
+       << "County Tax: " << county_tax << endl
+       << "Total Tax: " << sales_tax + county_tax;
+
+
+  cout << "Enter two values : ";
+  int x;
+  int y;
+  cin >> x >> y;
+  cout << x+y;
+  return 0;
+
+
+  cout << "Enter Fahrenheit value : ";
+  int fahrenheit;
+  cin >> fahrenheit;
+  double celcius = (fahrenheit - 32)/1.8;
+  cout << "Celcius : " << celcius;
+  return 0;
+
+  double res1 = floor(1.4);
+  cout << res1 << endl;
+  double res2 = pow(2,4);
+  cout << res2;
+  return 0;
+
+  float a = 9.99f;
+  long b = 10000L;
+  char l = 'a';
+  bool isValid = false;
+  auto valid = true;
+  int c {};
+  int d {1.2}; (will result in error)
+  cout << c;
+  return 0;
+
+  //Narrowing
+  int number = 1'00'000;
+  short another = number;
+  cout << another;
+  return 0;
+
+
+  long elapsedSeconds = time(0);
+  srand(elapsedSeconds);
+  int number = rand() % 10;
+  cout << number;
+  return 0;
+
+  str_t name = "BRO";
+  using namespace first;
+  cout << x;
+  return 0;
+
+
+  char x = (char) 100; //Explicit type conversion
+  cout << x;
+  return 0;
+
+  cout << "Name: ";
+  string name;
+  getline(cin >> ws, name);
+  cout << "Your name is: " << name;
+  return 0;
+
+
+  cout << "Enter your age: ";
+  int age;
+  cin >> age;
+  if (age >= 18){
+    cout << "Welcome to site!";
+  } else if (age < 0) {
+    cout << "Enter correct age!";
+  } else {
+    cout << "Access denied!";
+  }
+
+
+  cout << "Enter month number: ";
+  int month;
+  cin >> month;
+  //Alternative for multiple if statements
+  switch(month){
+    case 1:
+      cout << "January";
+      break;
+    case 2:
+      cout << "February";
+      break;
+    case 3:
+      cout << "March";
+      break;
+    //Soo onn...
+    default:
+      cout << "Invalid Number";
+      break;
+  }
+
+
+  int grade = 60;
+  //Ternary operator
+  grade > 80 ? cout << "Well Done" : cout << "Better luck next time";
+
+  */
+}
